d2e3fa7f2a8611240583e0b14bb6e1fa6f06f914
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-10-23 20:26:17 +0200

    Added CarType Entity and test

--- dao/src/main/java/com/jba/dao/cars/entity/CarType.java
+++ dao/src/main/java/com/jba/dao/cars/entity/CarType.java
@@ -1,5 +1,26 @@
 package com.jba.dao.cars.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+
+@Entity
+@Data
+@Table(name = "cartype")
 public class CarType {
-    //TODO: Implement
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PK_CAR_TYPE_ID")
+    private int carTypeId;
+
+    @Column(name = "CAR_TYPE_NAME")
+    private String carTypeName;
+
+    public CarType(){}
+
+    public CarType(String carTypeName){
+        this.carTypeName=carTypeName;
+    }
 }
