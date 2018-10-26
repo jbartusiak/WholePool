bb1a2f79c8ef4cd69c71bec9ff6b411a6004cd4a
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-10-26 18:33:00 +0200

    Added Route entity

--- dao/src/main/java/com/jba/dao/route/entity/Route.java
+++ dao/src/main/java/com/jba/dao/route/entity/Route.java
@@ -1,4 +1,44 @@
 package com.jba.dao.route.entity;
 
+import com.jba.dao.ride.enitity.Ride;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "route")
 public class Route {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PK_ROUTE_ID")
+    private int routeId;
+
+    @Column(name="ROUTE_FROM_LAT")
+    private float routeFromLatitude;
+
+    @Column(name="ROUTE_FROM_LONG")
+    private float routeFromLongitude;
+
+    @NonNull
+    @Column(name="ROUTE_FROM_LOCATION")
+    private String routeFromLocation;
+
+    @Column(name="ROUTE_TO_LAT")
+    private float routeToLatitude;
+
+    @Column(name="ROUTE_TO_LONG")
+    private float routeToLongitude;
+
+    @NonNull
+    @Column(name="ROUTE_TO_LOCATION")
+    private String routeToLocation;
+
+    @OneToMany()//TODO: add mapped by
+    @ToString.Exclude
+    private Set<Ride> ridesThatTakePlaceOnThisRoute;
 }
