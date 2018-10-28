1a9015f8da3bea3e36a12c668ed98c9cdc88df9c
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-10-28 18:03:44 +0100

    UsersPreferenceTest implemented

--- dao/src/test/java/com/jba/dao/preferences/entity/UsersPreferenceTest.java
+++ dao/src/test/java/com/jba/dao/preferences/entity/UsersPreferenceTest.java
@@ -1,18 +1,73 @@
 package com.jba.dao.preferences.entity;
 
+import com.jba.dao.user.enitity.User;
+import com.jba.session.WPLSessionFactory;
+import org.hibernate.Session;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//TODO Implement
 class UsersPreferenceTest {
+
+    private Session session;
+    private UsersPreference usersPreference;
 
     @BeforeEach
     void setUp() {
+        session = WPLSessionFactory.getDBSession();
+    }
+
+    @Test()
+    void testInOrder(){
+        createTest();
+        selectTest();
+        deleteTest();
+    }
+
+    void createTest(){
+        session.beginTransaction();
+        System.out.println("Get preference");
+        Preference preference = session.createQuery("from Preference where preferenceId=1", Preference.class).getSingleResult();
+        System.out.println("Get user");
+        User user = session.createQuery("from User where userId=1", User.class).getSingleResult();
+
+        usersPreference = new UsersPreference(user,preference,"true");
+
+        try {
+            System.out.println("Saving users preference");
+            session.save(usersPreference);
+            session.getTransaction().commit();
+        }
+        catch(Exception e){
+            session.getTransaction().rollback();
+            fail(e);
+        }
+
+        System.out.println("Save complete");
+    }
+
+    void selectTest(){
+        session.beginTransaction();
+
+        UsersPreference fromDB = session.createQuery("from UsersPreference where user.id=1 and preference.id=1", UsersPreference.class).getResultList().get(0);
+
+        assertEquals(usersPreference, fromDB);
+
+        session.getTransaction().commit();
+    }
+
+    void deleteTest(){
+        session.beginTransaction();
+
+        session.delete(usersPreference);
+
+        session.getTransaction().commit();
     }
 
     @AfterEach
     void tearDown() {
+        WPLSessionFactory.closeAndFinalize();
     }
 }
