5c4e5d454d894a29f3174f6acb59c669f26ffd39
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-10-28 19:24:49 +0100

    Route test

--- dao/src/test/java/com/jba/dao/route/entity/RouteTest.java
+++ dao/src/test/java/com/jba/dao/route/entity/RouteTest.java
@@ -1,18 +1,32 @@
 package com.jba.dao.route.entity;
 
+import com.jba.session.WPLSessionFactory;
+import org.hibernate.Session;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-//TODO: Implement
 class RouteTest {
+
+    private Session session;
 
     @BeforeEach
     void setUp() {
+        session = WPLSessionFactory.getDBSession();
+    }
+
+    @Test
+    void addNewEntryToRoute(){
+        Route route = new Route("adsfafafaf", "fafafafasas");
+        session.beginTransaction();
+        session.save(route);
+        session.getTransaction().commit();
     }
 
     @AfterEach
     void tearDown() {
+        WPLSessionFactory.closeAndFinalize();
     }
 }
