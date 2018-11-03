3fe1920f950199d11fab2725a6c4addccb4db095
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-11-03 23:07:09 +0100

    Further refactor for DAO classes

--- dao/src/main/java/com/jba/Main.java
+++
@@ -1,5 +0,0 @@
-package com.jba;
-
-public class Main {
-    
-}
--- dao2/src/test/java/com/jba/dao2/cars/entity/CarTest.java
+++ dao2/src/test/java/com/jba/dao2/cars/entity/CarTest.java
@@ -1,20 +1,33 @@
 package com.jba.dao2.cars.entity;
 
+import com.jba.dao2.Dao2Application;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Java6Assertions.fail;
 import static org.junit.Assert.assertEquals;
 
-class CarTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { Dao2Application.class })
+@WebAppConfiguration
+public class CarTest {
 
-    private Session session;
+    @Autowired
+    SessionFactory sessionFactory;
 
     @Test
-    void shouldNotCrashAndShowCollectionSize(){
-        session.beginTransaction();
+    @Transactional
+    public void shouldNotCrashAndShowCollectionSize(){
+        Session session = sessionFactory.getCurrentSession();
         int count = ((Long) session.createQuery("select count(*) from Car ").getSingleResult()).intValue();
         System.out.println("Select count(*) from Car result: "+count);
         List<Car> cars = session.createQuery("from Car ").getResultList();
@@ -23,12 +36,12 @@ class CarTest {
         cars.forEach(car-> System.out.println(car.toString()));
 
         assertEquals(count, cars.size());
-        session.getTransaction().commit();
     }
 
     @Test
-    void shouldSuccessfullyAddNewCarType(){
-        session.beginTransaction();
+    @Transactional
+    public void shouldSuccessfullyAddNewCarType(){
+        Session session = sessionFactory.getCurrentSession();
         CarType carType = session.createQuery("from CarType c where c.carTypeId=1", CarType.class).getSingleResult();
 
         Car newCarToInsert = new Car(carType, "My fancy car", "AKG123456789", 2015);
@@ -43,11 +56,7 @@ class CarTest {
             System.out.println("Fetched: " + fromDB.toString());
         }
         catch (Exception e){
-            if(session.getTransaction()!=null){
-                session.getTransaction().rollback();
-            }
             fail(e.getMessage());
         }
-        session.getTransaction().rollback();
     }
 }
