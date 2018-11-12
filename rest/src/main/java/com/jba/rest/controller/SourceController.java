64d1c28ac27824e1896a4649b183f9f0ff25f1b6
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-11-12 13:15:05 +0100

    Source controller swagger docs

--- rest/src/main/java/com/jba/rest/controller/SourceController.java
+++ rest/src/main/java/com/jba/rest/controller/SourceController.java
@@ -3,6 +3,8 @@ package com.jba.rest.controller;
 import com.jba.dao2.source.entity.Source;
 import com.jba.entity.WPLResponse;
 import com.jba.service.ifs.SourceService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +16,14 @@ public class SourceController {
     @Autowired
     private SourceService sourceService;
 
+    @ApiOperation(value = "Get source(s)", notes = "Fetches source(s) from the database. If optional parameter " +
+            "name is given, fetches details for that single source. If parameter is not provided, fetches all " +
+            "sources.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public WPLResponse getSource(
+            @ApiParam(name = "name", value="A String referring to Source entity", required = false,
+                    type = "String")
             @RequestParam(value = "name", required = false) String sourceName
     ){
         if(sourceName==null)
@@ -25,25 +32,34 @@ public class SourceController {
             return new WPLResponse<>(HttpStatus.OK, sourceService.getSourceByName(sourceName));
     }
 
+    @ApiOperation(value = "Add a new source", notes = "Adds a new source to the database", consumes = "application/json")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public WPLResponse addSource(
+            @ApiParam(name = "source", value="A JSON representing the Source entity", required = true,
+                    type = "application/json")
             @RequestBody(required = true) Source source
     ){
         return new WPLResponse<>(HttpStatus.CREATED, sourceService.addSource(source));
     }
 
+    @ApiOperation(value = "Updates a source", notes = "Updates a source in the database", consumes = "application/json")
     @PutMapping
     @ResponseStatus(HttpStatus.ACCEPTED)
     public WPLResponse editSource(
+            @ApiParam(name = "source", value="A JSON representing the Source entity", required = true,
+                    type = "application/json")
             @RequestBody(required = true) Source source
     ){
         return new WPLResponse<>(HttpStatus.ACCEPTED, sourceService.editSource(source));
     }
 
+    @ApiOperation(value = "Delete a source", notes = "Deletes a source from the database")
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteSource(
+            @ApiParam(name = "sourceId", value="An Integer referring to Source entity", required = true,
+                    type = "Integer")
             @RequestParam(required = true) Integer sourceId
     ){
         sourceService.deleteSource(Source.of(sourceId));
