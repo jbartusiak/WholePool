fa61f33df71a917f71334b539df360827c993e01
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-11-12 12:36:21 +0100

    Preference controller swagger docs

--- rest/src/main/java/com/jba/rest/controller/PreferenceController.java
+++ rest/src/main/java/com/jba/rest/controller/PreferenceController.java
@@ -2,6 +2,8 @@ package com.jba.rest.controller;
 
 import com.jba.entity.WPLResponse;
 import com.jba.service.ifs.PreferenceService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,9 +15,13 @@ public class PreferenceController {
     @Autowired
     private PreferenceService preferenceService;
 
+    @ApiOperation(value = "Get preference(s)", notes = "Gets preferences available in the system. If optional " +
+            "preferenceId parameter is given, returns details for that single preference.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public WPLResponse getPreferences(
+            @ApiParam(name = "id", value="An Integer referring to Preference entity", required = false,
+                    type = "Integer")
             @RequestParam(name = "id", required = false) Integer id
     ){
         if(id!=null)
