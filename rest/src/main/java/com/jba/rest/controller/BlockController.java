f5a58b395be3a9e51f91371116c4ac7262318bba
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-11-12 12:10:41 +0100

    Block controller swagger docs

--- rest/src/main/java/com/jba/rest/controller/BlockController.java
+++ rest/src/main/java/com/jba/rest/controller/BlockController.java
@@ -26,11 +26,11 @@ public class BlockController {
     }
 
     @ApiOperation(value = "Adds new lock type", notes = "Adds a new lock type to the system. Name of the lock type " +
-            "must be unique.", consumes = "BlockStatus")
+            "must be unique.", consumes = "application/json")
     @PostMapping("/type")
     @ResponseStatus(HttpStatus.CREATED)
     public WPLResponse addNewLockType(
-            @ApiParam(name="blockStatus", value = "JSON representing BlockStatus entity", required = true, type = "BlockStatus entity")
+            @ApiParam(name="blockStatus", value = "JSON representing BlockStatus entity", required = true, type = "application/json")
             @RequestBody(required = true) BlockStatus blockStatus
     ) {
         return new WPLResponse<>(HttpStatus.CREATED, lockService.addNewBlockStatus(blockStatus));
@@ -47,9 +47,12 @@ public class BlockController {
         lockService.deleteBlockStatus(lockTypeId);
     }
 
+    @ApiOperation(value = "Gets lock statuses", notes = "If param userId is present, fetches this user's block status. " +
+            "If param is absent, instead gets all current locks in the system.")
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     public WPLResponse getUsersLockStaus(
+            @ApiParam(name="userId", value="Integer referring to the primary key of user", required = false, type="Integer")
             @RequestParam(name = "userId", required = false) Integer userId
     ) {
         if (userId == null)
@@ -60,7 +63,9 @@ public class BlockController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
+    @ApiOperation(value = "Lock user", notes = "Locks a user with a given lock", consumes = "application/json")
     public WPLResponse lockUser(
+            @ApiParam(name="blockedUsers", value = "JSON representing the lock entity", required = true, type = "application/json")
             @RequestBody(required = true) BlockedUsers blockedUsers
     ) {
         return new WPLResponse<>(HttpStatus.CREATED, lockService.blockUser(
@@ -71,10 +76,15 @@ public class BlockController {
         ));
     }
 
+    @ApiOperation(value = "Remove user's lock", notes = "Removes user's lock, either normally or forcefully, if asked to." +
+            " Some locks in the system are permanent, but can be lifted via this method.")
     @DeleteMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void unlockUser(
+            @ApiParam(name="userId", value="Integer reffering to the primary key of user", required = true, type="Integer")
             @RequestParam(name = "userId", required = true) Integer userId,
+
+            @ApiParam(name="force", value="A boolean, which switches the forceful removal of lock", required = false, type="Boolean")
             @RequestParam(name = "force", required = false) Boolean force
     ) {
         if(force==null){
