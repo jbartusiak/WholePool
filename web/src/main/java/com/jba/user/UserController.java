984e33dddc7aff68debd7246fc89093a73fc625c
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-11-22 18:25:07 +0100

    Views for prefs and controllers added

--- web/src/main/java/com/jba/user/UserController.java
+++ web/src/main/java/com/jba/user/UserController.java
@@ -13,8 +13,37 @@ public class UserController {
         return "user-settings";
     }
 
+    @GetMapping("/settings/preferences")
+    public String getPreferencesSettings(){return "user-settings-preferences";}
+
     @GetMapping("/settings/data")
     public String getDataSettings(){
         return "user-settings-data";
+    }
+
+    @GetMapping("/settings/accountType")
+    public String getAccountType(){return "user-settings-accountType";}
+
+    @GetMapping("/settings/changePassword")
+    public String getChangePassword(){
+        return "user-settings-changePassword";
+    }
+
+    @GetMapping("/settings/myCars")
+    public String getCars(){return "user-settings-myCars";}
+
+    @GetMapping("/settings/addCar")
+    public String getAddCar(){
+        return "user-settings-addCar";
+    }
+
+    @GetMapping("/rides")
+    public String getUsersRides(){
+        return "user-rides";
+    }
+
+    @GetMapping("/searchHistory")
+    public String getUserSearchHistory(){
+        return "user-search-history";
     }
 }
