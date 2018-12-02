f1743dd1ad0839894a4775f8f83f8a63e0aa9962
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2018-12-02 20:02:48 +0100

    New Exception

--- web/src/main/java/com/jba/source/exception/MissingPropertiesException.java
+++ web/src/main/java/com/jba/source/exception/MissingPropertiesException.java
@@ -1,4 +1,21 @@
 package com.jba.source.exception;
 
-public class MissingPropertiesException {
+import java.io.IOException;
+
+public class MissingPropertiesException extends IOException {
+    public MissingPropertiesException() {
+        super();
+    }
+
+    public MissingPropertiesException(String message) {
+        super(message);
+    }
+
+    public MissingPropertiesException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MissingPropertiesException(Throwable cause) {
+        super(cause);
+    }
 }
